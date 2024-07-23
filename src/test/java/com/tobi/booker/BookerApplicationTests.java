dc76f63e482d58d0cadcddb2a8d7b22bc7a92488
Olutobi Ogunsola <olutobiogunsola@gmail.com>
2024-07-22 17:34:57 -0700

    initial commit

---
+++ src/test/java/com/tobi/booker/BookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobi.booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
